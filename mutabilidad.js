4117ebd20c99d5e6b12a70e62ca8dea247ba3714
EzeVillar <ezevillar2000@gmail.com>
2023-07-13 18:49:47 -0300

    desafio2

---
+++ mutabilidad.js
@@ -0,0 +1,11 @@
+const user = {
+    nombre : 'Eze'
+}
+
+// user = {
+//     precio : 100
+// };
+
+user.nombre = 'Pepe'
+
+console.log(user)
---
+++ tipoDatos.js
@@ -0,0 +1,18 @@
+let a = 'Eze'
+
+let b = 'Mishu'
+
+a =  b;
+
+console.log(a);
+
+const arrayA = [1,2,3];
+
+const arrayB = arrayA;
+
+arrayB.push (4); 
+
+console.log(arrayA)
+console.log(arrayB)
+
+
